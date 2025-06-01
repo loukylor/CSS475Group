9cbc5f8c8c23d026c7744bfe7be03d03c7c67f57
Kyler Li <46853114+loukylor@users.noreply.github.com>
2025-06-01 16:42:59 -0700

    Delete config.inc.php

--- config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost"; // tells mysqli to use socket
-$username = "root";
-$password = ""; 
-$database = "hiking_trials";
-$port = null;
-$socket = "/home/cssuwbstudent/mysql.sock"; // path from your mysqld startup
-?>
