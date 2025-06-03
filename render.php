828b142bf54abc2d8bc4220c53d1632c05a56772
Addison Douglas <addisondouglas850@gmail.com>
2025-06-03 00:26:48 +0000

    silly little varible

--- render.php
+++ render.php
@@ -1,6 +1,12 @@
 <?php
 
 function render_rows(string $sql_query, mysqli $conn, $get_row_func, &$bound_var, &...$bound_vars): void {
+    // Handle POST delete if requested
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['username'])) {
+        $username_to_delete = $_POST['username'];
+        delete_user_from_db($conn, $username_to_delete);
+    }
+    
     $stmt = $conn->stmt_init();
 
     if (!$stmt->prepare($sql_query)) {
@@ -18,18 +24,24 @@ function render_rows(string $sql_query, mysqli $conn, $get_row_func, &$bound_var
             }
             
             echo "<li style='display: flex; justify-content: space-between; align-items: center; flex-direction: row;'>
-        <div>
-            " . $get_row_func($clean_var, ...$clean_vars) . "
-        </div>
-        <form method='POST' action='delete_user.php' style='margin-left: 1em;'>
-            <input type='hidden' name='username' value='" . $clean_var . "'>
-            <button type='submit' class='delete-btn'>Delete</button>
-        </form>
-      </li>";
-
+            <div>" . $get_row_func($clean_var, ...$clean_vars) . "</div>
+            <form method='POST' style='margin-left: 1em;'>
+                <input type='hidden' name='username' value='" . $clean_var . "'>
+                <button type='submit' class='delete-btn'>Delete</button>
+            </form>
+          </li>";
         }
         echo "</ul>";
     }   
+}
+
+function delete_user_from_db(mysqli $conn, string $FirstName): void {
+    $stmt = $conn->prepare("DELETE FROM user WHERE FirstName = ?");
+    if ($stmt) {
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $stmt->close();
+    }
 }
 
 function get_row_title(string $column_data): string {
@@ -37,7 +49,7 @@ function get_row_title(string $column_data): string {
 }
 
 function get_row_sub(string $column_data): string {
-    return "<span class='role'>" . $column_data . "</span>";
+    return  $column_data;
 }
 
 function render_statement_fail(): void {
