417dad52ec440a9ce9de2193be8dd1bfb65775fb
Saurabh Vasdev <vasdev.saurabh@gmail.com>
2025-06-03 21:49:59 -0700

    Added About section

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ require_once 'header.inc.php';
 ?>
 <div>
     <h2>About</h2>
-    <div>This is sample PHP program that does simple database operations calling the mysqli library in PHP</div>
+    <div>We're the Hiking Trails Group. Group Members: Saurabh Vasdev, Tyler Ingram, Kyler Li, Addison Douglas</div>
 </div>
 </body>
 </html>
